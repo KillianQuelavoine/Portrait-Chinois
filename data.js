0b3e75f53935ae225c99099a14a9a99760b5444d
Killian Quelavoine <killian.quelavoine@gmail.com>
2022-02-01 14:37:55 +0100

    :sparkles: Addition of the file data.js, it contains the javascript code of the page

---
+++ data.js
@@ -0,0 +1,16 @@
+// var analogies
+// var analogies = ["un animal", "une couleur", "un pokemon", "une ville", "un élément", "une boisson", "un personnage de Disney" ];
+
+// console.log(analogies);
+
+// var numCase= 0;
+
+// analogies.forEach(function afficheNote(analogies) {
+//     console.log("Si j'étais " + analogies);
+//     document.querySelector('.chinois').innerHTML += "<h1 class='element'> Si j’étais " + analogie + ", je serais [une valeur de l’analogie].</h1>";
+
+//     //équivalent à numCase++
+
+//     numCase= numCase+1 
+//     console.log(numCase);
+// })
